8181a4314b7f3592668cab3bd99ad2bbc2f5c4c1
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-07-12 13:19:38 +0330

    Add Entity

---
+++ Asp_MongoDB/Asp_MongoDB/Entites/Student.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Asp_MongoDB.Entites
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid id { get; set; }
+
+        [BsonElement("FirstName")]
+        public string Name { get; set; }
+        public string BigName => Name.ToUpper();
+        public float Mark { get; set; }
+        public int Age { get; set; }
+
+    }
+}
