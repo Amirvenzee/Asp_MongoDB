03c2e247df733e6715c0519f0f6d3bff9497b7ba
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-07-12 13:44:51 +0330

    Add services For Employee

---
+++ Asp_MongoDB/Asp_MongoDB/Services/EmployeeService.cs
@@ -0,0 +1,41 @@
+﻿using Asp_MongoDB.Entites;
+using Asp_MongoDB.Models;
+using MongoDB.Driver;
+
+namespace Asp_MongoDB.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IMongoCollection<Employee> _employee;
+        public EmployeeService(MongoSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _employee = database.GetCollection<Employee>("Employees");
+        }
+        public async Task Create(Employee employee)
+        {
+             await _employee.InsertOneAsync(employee);
+        }
+
+        public async Task Delete(Guid Id)
+        {
+           await _employee.DeleteOneAsync(x => x.id == Id);
+        }
+
+        public async Task<List<Employee>> GetAll()
+        {
+            return await _employee.Find(x=> true).ToListAsync();
+        }
+
+        public async Task<Employee> GetById(Guid Id)
+        {
+            return await  _employee.Find(x => x.id == Id).FirstOrDefaultAsync();
+        }
+
+        public async Task Update(Employee employee)
+        {
+            await _employee.ReplaceOneAsync(x => x.id == employee.id, employee);
+        }
+    }
+}
---
+++ Asp_MongoDB/Asp_MongoDB/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using Asp_MongoDB.Entites;
+
+namespace Asp_MongoDB.Services
+{
+    public interface IEmployeeService
+    {
+        Task<List<Employee>> GetAll();
+        Task<Employee> GetById(Guid Id);
+        Task Delete(Guid Id);
+        Task Create(Employee employee);
+        Task Update(Employee employee);
+    }
+}
